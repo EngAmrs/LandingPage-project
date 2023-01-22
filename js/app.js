3914790203ee54dcb54600d1f20151f2dbcf1fc9
EngAmrs <amr.abdoullah@gmail.com>
2023-01-22 14:31:39 +0200

    Adding active class

--- js/app.js
+++ js/app.js
@@ -27,16 +27,36 @@ const Navbar = function BuildNAv (){
 }
 // Add class 'active' to section when near top of viewport
 
+function activeClass() {
+    
+    //For the nav buttons
+    let index = 0
+    //Iterate on sections and navbar buttons
+    getSections.forEach( targetSection => {
+        
+          const sectionTop = targetSection.getBoundingClientRect();
+          
+          if (sectionTop.top >= 0 && sectionTop.top < 300) {
+              targetSection.setAttribute('class','active');
+              getNavItems.getElementsByTagName("a")[index].setAttribute('class', 'btnActive');
+          } else {
+              targetSection.removeAttribute('class');
+              getNavItems.getElementsByTagName("a")[index].removeAttribute('class', 'btnActive');
+              
+          }
+          index++
+      });
+}
+
 // Scroll to anchor ID using scrollTO event
 function smoothScroll(e){
     getSectionId = e.target.getAttribute('data-SectionId');
     if(getSectionId){
         getSectionOffset = document.querySelector(getSectionId).offsetTop;
-
         //Window scroll
         window.scrollTo({
             top: getSectionOffset,
-            behavior: 'smooth'
+            behavior: 'smooth',
         });
     }
 }
@@ -49,9 +69,9 @@ function smoothScroll(e){
 
 // Build menu 
 window.onload = Navbar;
+
 // Scroll to section on link click
 getNavItems.addEventListener('click', smoothScroll);
 
 // Set sections as active
-
-
+window.addEventListener('scroll', activeClass);
